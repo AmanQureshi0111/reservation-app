3936882b9192d2c5efd84bda495814391206b9be
AmanQureshi0111 <amanq7362@gmail.com>
2025-09-27 12:50:45 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BpkText from "bpk-component-text";
+import BpkButton from "bpk-component-button";
+import BpkCalendar, { CALENDAR_SELECTION_TYPE } from "bpk-component-calendar";
+import { cssModules } from "bpk-react-utils";
+import STYLES from "./App.module.css";
+
+const getClassName = cssModules(STYLES);
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={getClassName("app")}>
+      <BpkText tagName="h1" textStyle="xxl">
+        Reservation Date
+      </BpkText>
+
+      <BpkCalendar
+        id="calendar"
+        onDateSelect={(date) => setSelectedDate(date)}
+        date={selectedDate}
+        selectionConfiguration={{
+          type: CALENDAR_SELECTION_TYPE.single,
+          date: selectedDate,
+        }}
+      />
+
+      <BpkButton
+        onClick={() => alert(`You selected: ${selectedDate}`)}
+        disabled={!selectedDate}
+      >
+        Continue
+      </BpkButton>
     </div>
   );
 }
